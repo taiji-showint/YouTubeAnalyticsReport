131b81352ca856d0990b782d38054bc54cc635bc
Taiji Tsuchiya @ show int <taiji@showint.net>
2024-09-23 06:40:43 +0900

    Go言語で書き直し中。YT_Data_API, YT_Analitcis_API

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"log"
-	"time"
+	"fmt"
+//	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -14,14 +15,27 @@ func main() {
 	}
 
 	//channel_stats := getChannelStats()
-	startdate := "2024-03-18T00:00:00Z"
-	enddate := "2024-05-27T23:00:00Z"
+	
+	//startdate := "2024-09-10T00:00:00Z"
+	//enddate := "2024-09-20T23:00:00Z"
 
-	today := time.Now().Format("20060102")
-	video_list := getVideoStats(startdate, enddate)
+	//today := time.Now().Format("20060102")
+	//video_list := getVideoStats(startdate, enddate)
+	
+	startdate := "2024-09-03"
+	enddate := "2024-09-20"
+	//video_analytics := getVideoTrafficSource("ePJIVZZhzRg")
+	//getVideoTrafficSource("ePJIVZZhzRg")
+	//fmt.println(video_analytics)
+
+	//getVideoBasicStats(startdate, enddate)
+	//fmt.Println(getVideoList(startdate, enddate))
+	//getVideoList(startdate, enddate)
+	video_list := gatherVideoStats(startdate, enddate)
+	fmt.Println(video_list)
 
 	// 通常レポート(yearly=false)
-	render_report(video_list, today, false)
+	//render_report(video_list, today, false)
 
 	// 年間レポート(yearly=true)
 	//render_report(video_list, today, true)
--- youtube.go
+++ youtube.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 	"strings"
+	"encoding/json"
 
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
+	"google.golang.org/api/youtubeanalytics/v2"
+	
 )
 
 type ChannelStats struct {
@@ -23,11 +27,18 @@ type Video struct {
 	Video_title    string
 	Video_id       string
 	Updated_date   string
-	View_counts    uint64
-	Like_counts    uint64
-	Dislike_counts uint64
+	View_counts    float64
+	Like_counts    float64
+	Dislike_counts float64
 	thumbnail_url  string
 }
+
+//type Response interface {}
+
+const (
+	CHANNEL_ID_QUERY =  "channel==MINE"
+	STARTDATE_SHOWINT = "2019-01-01"
+)
 
 func reverseVideoList(v []Video) []Video {
 	for i, j := 0, len(v)-1; i < j; i, j = i+1, j-1 {
@@ -90,6 +101,198 @@ func getChannelStats() ChannelStats {
 	return channel_stats
 }
 
+// YouTube Data API
+// Official Reference: https://developers.google.com/youtube/v3/getting-started?hl=ja
+// Official Sample: https://developers.google.com/youtube/v3/code_samples/go
+// Library Source Code: https://github.com/googleapis/google-api-go-client/blob/main/youtube/v3/youtube-gen.go
+func callYTDataAPI(
+	startdate string,
+	enddate string,
+	maxresult int64,
+) *youtube.SearchListResponse {
+	API_KEY := os.Getenv("API_KEY")
+	CHANNEL_ID := os.Getenv("CHANNEL_ID")
+	
+	// New() は非推奨になってた。Deprecated: please use NewService instead.とのこと
+	// https://github.com/googleapis/google-api-go-client/blob/d0e0dc31cd30ec9b5e71541ad905236401b56d96/youtube/v3/youtube-gen.go#L159-L163
+	//service, err := youtube.New(client)
+
+	ctx := context.Background()
+	service, err := youtube.NewService(ctx, option.WithAPIKey(API_KEY))
+	if err != nil {
+		log.Fatalf("Error creating new YouTube service: %v", err)
+	}
+
+	startdate += "T00:00:00Z"
+	enddate += "T23:59:59Z"
+
+	call := service.Search.List([]string{"id", "snippet"}).
+		ChannelId(CHANNEL_ID).
+		MaxResults(maxresult).
+		Order("date").
+		PublishedAfter(startdate).
+		PublishedBefore(enddate)
+
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalf("Error making YouTube Data API call : %v", err)
+	}
+
+	return response
+}
+
+// YouTube Analytics API
+// Official Reference: https://developers.google.com/youtube/reporting
+// Official Sample: https://github.com/youtube/api-samples/tree/07263305b59a7c3275bc7e925f9ce6cabf774022/python
+// Sample API call : https://developers.google.com/youtube/analytics/sample-requests#channel-geographic-reports
+// Good blog: https://blog.codecamp.jp/youtube-analytics-python
+// Google標準APIライブラリでは、 YouTube Data API のみ対応していて、AnalyticsAPIは呼び出し対応していなそうだった。
+// 別のライブラリ google.golang.org/api/youtubeanalytics/v2 であれば、YouTube AnalyticsAPIを呼び出せそうだ。
+// https://pkg.go.dev/google.golang.org/api/youtubeanalytics/v2#pkg-functions
+func callYTAnalyticsAPI(
+	dimentions string,
+	metrics string,
+	filters string, 
+	startdate string,
+	enddate string,
+	sort string,
+	maxresult int64,
+) *youtubeanalytics.QueryResponse {
+	client := getClient(youtubeanalytics.YtAnalyticsReadonlyScope)
+	service, err := youtubeanalytics.New(client)
+
+	call := service.Reports.Query().
+		Ids(CHANNEL_ID_QUERY).
+		Dimensions(dimentions).
+		Metrics(metrics).
+		StartDate(startdate).
+		EndDate(enddate).
+		MaxResults(maxresult).
+		Sort(sort).
+		Filters(filters)
+
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalf("Error making YouTube Analytics API call: %v", err)
+	}
+	return response
+}
+
+func getVideoList(startdate string, enddate string) []Video {
+	response := callYTDataAPI(
+		startdate, // startdate string
+		enddate, // enddate string
+		15, // maxresult int64
+	)
+	
+	//fmt.Println(response)
+	//fmt.Printf("%T\n", response)
+	//response_json, _ := json.MarshalIndent(response,"","    ")
+	//fmt.Println(string(response_json))
+
+	var video Video
+	var video_list []Video
+
+	for _, item := range response.Items {
+		video.Video_title = item.Snippet.Title
+		video.Video_id = item.Id.VideoId
+		video.Updated_date = strings.Split(item.Snippet.PublishedAt, "T")[0]
+		video.thumbnail_url = item.Snippet.Thumbnails.High.Url
+		video_list = append(video_list, video)
+	}
+	return reverseVideoList(video_list)
+}
+
+func updateVideoCount(video Video) {
+	enddate_today := time.Now().Format("2006-01-02")
+	//fmt.Println(enddate_today)
+	filter_query := fmt.Sprintf("video==%s", video.Video_id)
+	
+	response := callYTAnalyticsAPI(
+		"video", // dimentions
+		"views,likes,dislikes", // metrics
+		filter_query, // filters
+		STARTDATE_SHOWINT, // startdate
+		enddate_today, // enddate
+		"", // sort
+		5, // maxresult
+	)
+
+	video.View_counts = response.Rows[0][1].(float64)
+	video.Like_counts = response.Rows[0][2].(float64)
+	video.Dislike_counts = response.Rows[0][3].(float64)
+
+	//filter_query := fmt.Sprintf("video==%s;insightTrafficSourceType==EXT_URL", video.Video_id)
+	//response := callYTAnalyticsAPI(
+	//	"insightTrafficSourceDetail", // dimentions
+	//	"estimatedMinutesWatched,views", // metrics
+	//	filter_query, // filters
+	//	"2024-09-10", // startdate
+	//	"2024-09-20", // enddate
+	//	"-estimatedMinutesWatched", // sort
+	//	10, // maxresult
+	//)
+	
+	//fmt.Println(response)
+	//fmt.Printf("%T\n", response)
+	//fmt.Println(response.Rows)
+	fmt.Println(video)
+	fmt.Println(response.Rows[0][1].(float64))
+	fmt.Println(video.View_counts)
+	fmt.Printf("%T\n", response.Rows[0][1])
+	//m, _ := json.MarshalIndent(response,"","    ")
+	//fmt.Println(string(m))
+
+}
+
+// func getVideoTrafficSource(video_id string) []VideoTrafficSrouce {
+func updateVideoTrafficSource(video Video) {
+
+	filter_query := fmt.Sprintf("video==%s;insightTrafficSourceType==EXT_URL", video.Video_id)
+	response := callYTAnalyticsAPI(
+		"insightTrafficSourceDetail", // dimentions
+		"estimatedMinutesWatched,views", // metrics
+		filter_query, // filters
+		"2024-09-10", // startdate
+		"2024-09-20", // enddate
+		"-estimatedMinutesWatched", // sort
+		10, // maxresult
+	)
+	//response := callYTAnalytics(
+	//  channel_id: "channel==MINE",
+	//	dimentions: "insightTrafficSourceDetail",
+	//	metrics: "estimatedMinutesWatched,views",
+	//	filters: "video==ePJIVZZhzRg;insightTrafficSourceType==EXT_URL",
+	//	startdate: "2024-09-10",
+	//	enddate: "2024-09-20",
+	//	sort: "-estimatedMinutesWatched",
+	//	maxresult: 10,
+	//)
+	//
+
+	//fmt.Println(response)
+	m, _ := json.MarshalIndent(response,"","    ")
+	fmt.Println(string(m))
+
+	//return response
+}
+
+func gatherVideoStats(startdate string, enddate string) []Video {
+	video_list := getVideoList(startdate, enddate)
+
+	//fmt.Println(video_list)
+	//fmt.Printf("%T\n", video_list)
+
+	for _, video := range video_list {
+		updateVideoCount(video)
+		fmt.Println(video.View_counts)
+	}
+
+	fmt.Println(video_list)
+	return video_list
+} 
+
+/*
 func getVideoStats(startdate string, enddate string) []Video {
 
 	API_KEY := os.Getenv("API_KEY")
@@ -185,3 +388,4 @@ func getVideoStats(startdate string, enddate string) []Video {
 	video_list = reverseVideoList(video_list)
 	return video_list
 }
+*/
